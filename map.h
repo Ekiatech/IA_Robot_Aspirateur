22516cdae9e0d9b4d6aeb15695f2f22c79628d0c
Mathilde <mathilde.pommier.13@gmail.com>
2022-09-27 11:52:57 -0400

    Init map with 20% of objects (dust or jewel)

--- map.h
+++ map.h
@@ -30,14 +30,19 @@ struct Room create_empty_room(int coordinates[2]);
 
 struct Map * create_empty_map(int size, struct Robot * robot);
 
+void init_map(struct Map * map);
+
 void free_map(struct Map * map);
+
+int choose_dust_or_jewel();
 
 int choose_object();
 
 int int_in_array(int value, int array[], int size);
 
-void gen_random_object(struct Map * map);
+int place_object(struct Map * map, int x_room, int y_room, int object, int object_has_been_added);
 
+void gen_random_object(struct Map * map);
 
 void clean(struct Map * map);
 
